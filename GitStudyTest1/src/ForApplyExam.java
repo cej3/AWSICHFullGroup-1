4aadbadf471329d74c5c395add9fb9a7defb3c1e
EZEN-140 <EZEN-140@DESKTOP-1IKNAEC>
2022-07-13 20:15:15 +0900

    충돌　해결하려고　노력중

--- GitStudyTest1/src/ForApplyExam.java
+++ GitStudyTest1/src/ForApplyExam.java
@@ -1,3 +1,4 @@
+//짝수들의 합을 구하는 프로그램 단, 깃허브가 최신의 내용이다.
 public class ForApplyExam {
 
 	public static void main(String[] args) {
@@ -7,7 +8,7 @@ public class ForApplyExam {
 		
 		for (int i = 0; i <= 10; i++) {
 //			System.out.println("i가 2인 경우에만 수행되는 조건문");
-			
+			if(i == 2) 
 				if(i%2 == 0){
 				sum = sum + i;
 				System.out.println("i의 현재 위치: " + i + " - 합계: " + sum);
